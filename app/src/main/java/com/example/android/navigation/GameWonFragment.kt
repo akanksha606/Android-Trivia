a4840392338eb1d52f675b44e293e3c21ac464e9
akanksha606 <akankshagupta98583@gmail.com>
2020-05-29 00:40:26 +0530

    Android Trivia

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,9 +16,11 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -44,5 +46,28 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.winner_menu,menu)
+        //check if activity resolves
+        if(null==getShareIntent().resolveActivity(activity!!.packageManager)){
+         //hide share menu item if activity not resolved
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+    private fun getShareIntent():Intent{
+        var args=GameWonFragmentArgs.fromBundle(arguments!!)
+        return ShareCompat.IntentBuilder.from(activity).setText(getString(R.string.share_success_text,
+                            args.numCorrect,args.numQuestions))
+                .setType("text/plain")
+                .intent
+
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share->shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
